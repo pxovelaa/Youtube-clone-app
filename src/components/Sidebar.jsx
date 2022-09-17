73e32eae50beb6330e5d77a6a4e68de9d86cc41e
pxovelaa <gio.pxovelishvili@gmail.com>
2022-09-17 05:58:04 +0300

    prettier all code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
+import React from "react";
 import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
 
-
-const Sidebar = ({selectedCategory, setSelectedCategory}) => (
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -17,22 +17,15 @@ const Sidebar = ({selectedCategory, setSelectedCategory}) => (
         className="category-btn"
         onClick={() => setSelectedCategory(category.name)}
         style={{
-          background: category.name === selectedCategory && "#fc1503",
+          background: category.name === selectedCategory && "#FC1503",
           color: "white",
         }}
         key={category.name}
       >
-        <span
-          style={{
-            color: category.name === selectedCategory ? "white" : "red",
-            marginRight: "15px",
-          }}
-        >
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
           {category.icon}
         </span>
-        <span
-          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
-        >
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
           {category.name}
         </span>
       </button>
@@ -40,4 +33,4 @@ const Sidebar = ({selectedCategory, setSelectedCategory}) => (
   </Stack>
 );
 
-export default Sidebar;
+export default Categories;
